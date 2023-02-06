ed69dd9f5b4bcd861119026509b0e60c0167f1ee
Mehdi Azouz <mehdiazouz@MacBook-Air-de-Mehdi.local>
2023-02-06 14:28:35 +0100

    ajout code product n2

--- front/js/product.js
+++ front/js/product.js
@@ -33,3 +33,50 @@ function ajax(url) {
     });
 }
 ajax(url);
+
+/**
+ * Replissage de la page article avec les infos récupérer via l'id renseigné
+ * @param { String } imageUrl
+ * @param { String } imageAlt
+ * @param { String } name
+ * @param { String } price
+ * @param { String } description
+ * @param { String } colors
+ **/
+function createArticlePage(
+  imageUrl,
+  imageAlt,
+  name,
+  price,
+  description,
+  colors
+) {
+  //   modification du titre de la page
+  document.title = name;
+
+  //   ajout de l'image de l'article
+  let item__img = document.getElementsByClassName("item__img");
+  let pictureItem = document.createElement("img");
+  pictureItem.src = imageUrl;
+  pictureItem.alt = imageAlt;
+  item__img[0].append(pictureItem);
+
+  //   ajout du titre de l'article
+  document.getElementById("title").textContent = name;
+
+  //   ajout du prix de l'article
+  document.getElementById("price").textContent = price;
+
+  //   ajout de la description de l'article
+  document.getElementById("description").textContent = description;
+
+  //   ajout des couleurs disponibles
+  let colorSelector = document.getElementById("colors");
+
+  for (let color of colors) {
+    let optionColor = document.createElement("option");
+    optionColor.value = color;
+    optionColor.textContent = color;
+    colorSelector.append(optionColor);
+  }
+}
